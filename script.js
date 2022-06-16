007bbccb2ff4390507a1772175b449f54290d148
ornel77 <t.ornel@yahoo.fr>
2022-06-16 11:20:32 +0200

    create object with 3 arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+/* --------------------- Object containing the 3 arrays --------------------- */
+const wordList = {
+    players: ['Stephen Curry', 'Luka Doncic', 'Jayson Tatum', 'Dejounte Murray'],
+    foods: ['pizza', 'ravitoto', 'pasta all carbonara', 'pie'],
+    funnySentences: ['fly off the handle', 'be a happy camper', 'move it or lose it', 'keep a cool head'], 
+}
+
+console.log(`Today you ${wordList.players[3]} and you like ${wordList.foods[1]}.\nTommorow wil be the time to ${wordList.funnySentences[2]}.`)
