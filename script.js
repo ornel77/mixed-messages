4b9cb3377b1effcd8735a4d2a6d504483ea7bfde
ornel77 <t.ornel@yahoo.fr>
2022-06-16 15:05:54 +0200

    scipt finish

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 /* --------------------- Object containing the 3 arrays --------------------- */
 const wordList = {
-    players: ['Stephen Curry', 'Luka Doncic', 'Jayson Tatum', 'Dejounte Murray'],
-    foods: ['pizza', 'ravitoto', 'pasta all carbonara', 'pie'],
+    players: ['Stephen Curry', 'Luka Doncic', 'Jayson Tatum', 'Dejounte Murray', 'Tony Parker'],
+    foods: ['pizza', 'ravitoto', 'pasta alla carbonara', 'pie'],
     funnySentences: ['fly off the handle', 'be a happy camper', 'move it or lose it', 'keep a cool head'], 
 }
 
-console.log(`Today you ${wordList.players[3]} and you like ${wordList.foods[1]}.\nTommorow wil be the time to ${wordList.funnySentences[2]}.`)
+/* ---------- function to generate a random index based on an array --------- */
+const generateIndex = num => {
+    let index = Math.floor(Math.random() * num)
+    return index
+}
+
+let finalSentence = []
+for(let list in wordList) {
+    const randomIndex = generateIndex(wordList[list].length)
+    switch(list) {
+        case 'players':
+            finalSentence.push(`Today you are ${wordList[list][randomIndex]} `)
+            break;
+        case 'foods':
+            finalSentence.push(`and you like ${wordList[list][randomIndex]}.\n`)
+            break;
+        case 'funnySentences':
+            finalSentence.push(`Tommorow will be the time to ${wordList[list][randomIndex]}.`)
+            break;
+        default:
+            console.log('not defined')
+            break;
+    }
+}
+
+console.log(finalSentence.join(''))
